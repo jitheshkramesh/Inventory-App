20cb2f9ba7193e0ed0994862ce1e76f52a0bc27f
JitheshKRamesh <jitheshkramesh@gmail.com>
2019-06-30 11:26:00 +0400

    Second commit 5

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/interface/product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() product: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productservice: ProductService) { }
 
   ngOnInit() {
+    let id = + this.route.snapshot.params['id'];
+
+    this.productservice.getProductById(id).subscribe(result => this.product = result);
   }
 
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, TemplateRef, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Product } from 'src/app/interface/product';
@@ -14,7 +14,7 @@ import { Router } from '@angular/router';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   // For the FormControl - Adding products
   insertForm: FormGroup;
@@ -65,11 +65,112 @@ export class ProductListComponent implements OnInit {
     this.modalRef = this.modalService.show(this.modal);
   }
 
+  // Method to Add new Product
+  onSubmit() {
+    let newProduct = this.insertForm.value;
+
+    this.productservice.insertProduct(newProduct).subscribe(
+      result => {
+        this.productservice.clearCache();
+        this.products$ = this.productservice.getProducts();
+
+        this.products$.subscribe(newlist => {
+          this.products = newlist;
+          this.modalRef.hide();
+          this.insertForm.reset();
+          this.rerender();
+
+        });
+        console.log("New Product added");
+
+      },
+      error => console.log('Could not add Product')
+
+    )
+
+  }
+
+  // We will use this method to destroy old table and re-render new table
+
+  rerender() {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first in the current context
+      dtInstance.destroy();
+
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next();
+
+    });
+  }
+
+  // Update an Existing Product
+  onUpdate() {
+    let editProduct = this.updateForm.value;
+    this.productservice.updateProduct(editProduct.id, editProduct).subscribe(
+      result => {
+        console.log('Product Updated');
+        this.productservice.clearCache();
+        this.products$ = this.productservice.getProducts();
+        this.products$.subscribe(updatedlist => {
+          this.products = updatedlist;
+
+          this.modalRef.hide();
+          this.rerender();
+        });
+      },
+      error => console.log('Could Not Update Product')
+    )
+  }
+
+  // Load the update Modal
+
+  onUpdateModal(productEdit: Product): void {
+    this._id.setValue(productEdit.productId);
+    this._name.setValue(productEdit.name);
+    this._price.setValue(productEdit.price);
+    this._description.setValue(productEdit.description);
+    this._imageUrl.setValue(productEdit.imageUrl);
+
+    this.updateForm.setValue({
+      'id': this._id.value,
+      'name': this._name.value,
+      'price': this._price.value,
+      'description': this._description.value,
+      'imageUrl': this._imageUrl.value,
+      'outOfStock': true
+    });
+
+    this.modalRef = this.modalService.show(this.editmodal);
+
+  }
+
+  // Method to Delete the product
+  onDelete(product: Product): void {
+    this.productservice.deleteProduct(product.productId).subscribe(result => {
+      this.productservice.clearCache();
+      this.products$ = this.productservice.getProducts();
+      this.products$.subscribe(newlist => {
+        this.products = newlist;
+
+        this.rerender();
+      })
+    })
+  }
+
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+
+    this.router.navigateByUrl("/products/" + product.productId);
+  }
+
   ngOnInit() {
+
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 9,
       autoWidth: true,
+      retrieve: true,
+      paging: false,
       order: [[0, 'desc']]
     };
 
@@ -78,7 +179,7 @@ export class ProductListComponent implements OnInit {
     this.products$.subscribe(result => {
       this.products = result;
 
-      //this.chRef.detectChanges();
+      this.chRef.detectChanges();
 
       this.dtTrigger.next();
     });
@@ -88,7 +189,7 @@ export class ProductListComponent implements OnInit {
       this.dtTrigger.next();
     });
 
-    this.acct.currentUserRole.subscribe(result => {this.userRoleStatus = result});
+    this.acct.currentUserRole.subscribe(result => { this.userRoleStatus = result });
 
 
     // Modal Message
@@ -102,37 +203,40 @@ export class ProductListComponent implements OnInit {
     this.price = new FormControl('', [Validators.required, Validators.min(0), Validators.max(10000)]);
     this.description = new FormControl('', [Validators.required, Validators.maxLength(150)]);
     this.imageUrl = new FormControl('', [Validators.pattern(validateImageUrl)]);
-    
+
     this.insertForm = this.fb.group({
 
-            'name' : this.name,
-            'price' : this.price,
-            'description' : this.description,
-            'imageUrl' : this.imageUrl,
-            'outOfStock' : true,
-    
-            });
+      'name': this.name,
+      'price': this.price,
+      'description': this.description,
+      'imageUrl': this.imageUrl,
+      'outOfStock': true,
+
+    });
 
     // Initializing Update Product properties
-    this._name = new FormControl('',[Validators.required, Validators.maxLength(50)]); 
+    this._name = new FormControl('', [Validators.required, Validators.maxLength(50)]);
     this._price = new FormControl('', [Validators.required, Validators.min(0), Validators.max(10000)]);
     this._description = new FormControl('', [Validators.required, Validators.maxLength(150)]);
     this._imageUrl = new FormControl('', [Validators.pattern(validateImageUrl)]);
     this._id = new FormControl();
 
     this.updateForm = this.fb.group(
-        {
-            'id' : this._id,
-            'name' : this._name,
-            'price' : this._price,
-            'description' : this._description,
-            'imageUrl' : this._imageUrl,
-            'outOfStock' : true
+      {
+        'id': this._id,
+        'name': this._name,
+        'price': this._price,
+        'description': this._description,
+        'imageUrl': this._imageUrl,
+        'outOfStock': true
 
-        });
+      });
 
   }
 
-
+  ngOnDestroy() {
+    // Do not forget to unsubscribe
+    this.dtTrigger.unsubscribe();
+  }
 
 }
